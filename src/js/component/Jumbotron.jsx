6022dffa089f6403228d5b9ae101331f165bd002
Adrian <142854436+withyso@users.noreply.github.com>
2024-04-05 15:56:11 +0000

    NavBar component done, Jumbotron component in progress

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Jumbotron = () => {
+
+    return(
+        <React.Fragment>
+            <div className="py-5 ps-3 pe-3 m-auto bg-light">
+                Soy un jumbotron
+            </div>
+        </React.Fragment>
+    );
+}
